e25fde666e4ff1d5ae49a48cf8cec81c91275cf7
sohn12 <gantalasohanreddy@gmail.com>
2019-09-21 15:45:38 +0530

    Added Likes Functionality

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -18,8 +18,10 @@ namespace DatingApp.API.Models
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
-        public string Country { get; set; } 
+        public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<Like> Likers { get; set; }
+        public ICollection<Like> Likees { get; set; }
 
     }
 }
